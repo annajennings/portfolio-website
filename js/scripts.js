6e169aa4a9cba39bfb8978760f951cadca236eb9
annajennings <112507127+annajennings@users.noreply.github.com>
2022-09-22 17:57:52 -0700

    corrected JS errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-console.log("My Portfolio")
-function menuToggle() {
-  var x = document.getElementById("myNavtoggle");
-  if (x.className === "navtoggle") {
-  x.className += " responsive";
-} else {
-  x.className = "navtoggle"
-}
+console.log('My Portfolio')
+
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
 }
