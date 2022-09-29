d40ca423a3a8141a4e979487230d3dadd205d32a
annajennings <112507127+annajennings@users.noreply.github.com>
2022-09-29 08:49:18 -0700

    linter fixes

--- js/scripts.js
+++ js/scripts.js
@@ -10,47 +10,74 @@ function menuToggle () {
 }
 
 // Open the Modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+function openModal () {
+  document.getElementById('myModal').style.display = ' block'
 }
 
 // Close the Modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+function closeModal () {
+  document.getElementById('myModal').style.display = 'none'
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 1
+showSlides(slideIndex)
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides (n) {
+  howSlides (slideIndex += n)
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide (n) {
+  mySlides (slideIndex = n)
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+function showSlides (n) {
+  var i
+  var slides = document.getElementsByClassName('mySlides')
+  var dots = document.getElementsByClassName('demo')
+  var captionText = document.getElementById('caption')
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none'
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace('active', '')
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex - 1].style.display = 'block'
+  dots[slideIndex - 1].className += 'active'
+  captionText.innerHTML = dots[slideIndex - 1].alt
 }
-let mainNavLinks = document.querySelectorAll("nav ul li a");
-let mainSections = document.querySelectorAll("main section");
+let mainNavLinks = document.querySelectorAll('nav ul li a')
+let mainSections = document.querySelectorAll('main section')
 
-let lastId;
-let cur = [];
+let lastId
+let cur = []
+
+
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+//    _.throttle(doThatStuff, 100);
+// });
+// Only not doing it here to keep this Pen dependency-free.
+
+window.addEventListener('scroll', event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add('current')
+    } else {
+      link.classList.remove('current')
+    }
+  })
+}
