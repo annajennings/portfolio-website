d0178fda2261c686f796d6aaefcd28e3c60264bd
annajennings <112507127+annajennings@users.noreply.github.com>
2022-09-29 18:06:45 -0700

    added slide show to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,7 @@ function plusSlides (n) {
 }
 
 // Thumbnail image controls
+
 function currentSlide (n) {
   mySlides (slideIndex = n)
 }
@@ -55,7 +56,34 @@ let mainSections = document.querySelectorAll('main section')
 let lastId
 let cur = []
 
+//Slideshowlet slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 // This should probably be throttled.
 // Especially because it triggers during smooth scrolling.
 // https://lodash.com/docs/4.17.10#throttle
